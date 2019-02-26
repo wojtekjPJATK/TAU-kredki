31c741b5ee8809045c021f3b6e862a1679fd7fb1
wojtekj <s15983@pjwstk.edu.pl>
2019-02-26 09:49:53 +0100

    Pierwsze laby

---
+++ src/main/java/pl/edu/pjatk/tau/lab1/Calculator.java
@@ -0,0 +1,20 @@
+package pl.edu.pjatk.tau.lab1;
+
+/**
+ * Hello world!
+ *
+ */
+public class Calculator 
+{
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    public Double sumOfTen() {
+        Double sum = 0.0;
+        for(int i = 0; i < 10; i++) {
+            sum += 0.1;
+        }
+        return sum;
+    }
+}
