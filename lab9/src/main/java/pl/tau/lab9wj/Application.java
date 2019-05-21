a5da057796010e392fba9bfd9e97bea02350f075
wojtekj <s15983@pjwstk.edu.pl>
2019-05-21 08:05:21 +0200

    adding missing files to the repo

---
+++ lab9/src/main/java/pl/tau/lab9wj/Application.java
@@ -0,0 +1,22 @@
+package pl.tau.lab9wj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootApplication
+@Configuration
+@PropertySource("classpath:jdbc.properties")
+@ImportResource({"classpath:/beans.xml"})
+@ComponentScan({"pl.lab11"})
+@Transactional ()
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
